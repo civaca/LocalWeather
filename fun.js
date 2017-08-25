75bff2d1ec94ad2319ee99f0460a2175b4ba4655
civaca <gemelosvs@gmail.com>
2017-08-24 23:02:00 -0500

    More functions

--- fun.js
+++ fun.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event){
+   
+    
     var x;
     var y;
     
     if(navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(showPosition);
-      } else {document.getElementById("temp").innerHTML = "No supported"}
+      } else {document.getElementById("temp").innerHTML = "No supported";}
     }); /*closure of loaded*/
+
+
     function showPosition(position){
         x= position.coords.latitude;
         y= position.coords.longitude;
-         console.log(x);
-    console.log(y);
-        
+                 
     var ajax =  new XMLHttpRequest();
     
    ajax.open("GET", "https://fcc-weather-api.glitch.me/api/current?lat="+x+"&lon="+y,true);
@@ -21,11 +23,20 @@ document.addEventListener("DOMContentLoaded", function(event){
     if (this.readyState == 4 && this.status == 200){
         var newAjax = JSON.parse(this.responseText);
       console.log(newAjax);
-      document.getElementById("temp").innerHTML = newAjax.name
-    }; /* closure of if*/
+      document.getElementById("name").innerHTML = newAjax.name;
+        document.getElementById("temp").innerHTML = newAjax.main.temp_max;
+        document.getElementById("graph").src = newAjax.weather[0].icon;
+        document.getElementById("change").addEventListener("click",function changeToFr(){
+        document.getElementById("temp").innerHTML== newAjax.main.temp_max ?  document.getElementById("temp").innerHTML = (newAjax.main.temp_max*(9/5))+32 :
+        document.getElementById("temp").innerHTML = newAjax.main.temp_max
+        ;
+    
+        }); /* closure of change*/
+    } /* closure of if*/
     
  }; /*closure of readstatechange*/
   
 
   
-}; /* closure of position*/
+} /* closure of position*/
+
